39e4bf0dcb7905239f9271a92f7ebc397a205f50
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-01-27 11:50:02 +0700

    Added flags to the main cli binary

--- cmd/shipper/main.go
+++ cmd/shipper/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"sort"
 
 	"github.com/urfave/cli"
 
@@ -23,16 +24,33 @@ func Init() *cli.App {
 	app.Name = config.Name()
 	app.Version = config.Version()
 	app.Usage = "This service ships binaries to the server"
+	app.Action = func(c *cli.Context) error {
+		logger.Infoln("Getting ready to ship")
+		logger.Infoln(c.String("key"))
+		logger.Infoln(c.String("file"))
+		logger.Infoln(c.String("bundle"))
+		return nil
+	}
 
-	app.Commands = []cli.Command{
-		{
-			Name:  "start",
-			Usage: "start the service",
-			Action: func(c *cli.Context) error {
-				return nil
-			},
+	app.Commands = []cli.Command{}
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "key, k",
+			Usage: "access key for authentication",
+		},
+		cli.StringFlag{
+			Name:  "file, f",
+			Usage: "file to be uploaded",
+		},
+		cli.StringFlag{
+			Name:  "bundle, b",
+			Usage: "app bundle to link to",
 		},
 	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
 
 	if err := app.Run(os.Args); err != nil {
 		panic(err)
