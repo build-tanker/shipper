4fe58eff29f929265183f92abd9eae1571012414
Sudhanshu Raheja <sudhanshu@go-jek.com>
2018-02-17 10:51:19 +0700

    Added tests for loggers

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -12,7 +12,14 @@ import (
 func TestLogger(t *testing.T) {
 	conf := config.NewConfig([]string{"$HOME"})
 	var b bytes.Buffer
+
 	log := NewLogger(conf, &b)
-	log.Errorln("Testing", "hello")
-	assert.Contains(t, b.String(), "Testing hello")
+	log.Debugln("Testing", "Debug")
+	assert.Contains(t, b.String(), "Testing Debug")
+
+	log.Errorln("Testing", "Error")
+	assert.Contains(t, b.String(), "Testing Error")
+
+	log.Infoln("Testing", "Info")
+	assert.Contains(t, b.String(), "Testing Info")
 }
